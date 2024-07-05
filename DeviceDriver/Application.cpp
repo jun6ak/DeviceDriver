41b46384f2bb9232e9357f2da655ce8bfa58cc89
junyoung <jun6ak@gmail.com>
2024-07-05 11:05:41 +0900

    [feature] application 생성

---
+++ DeviceDriver/Application.cpp
@@ -0,0 +1,17 @@
+#include "Application.h"
+
+void Application::ReadAndPrint(long startAddr, long endAddr)
+{
+    /*
+        ✔ ReadAndPrint(startAddr, endAddr)
+        ▪ startAddr ~ endAddr 까지 Read 수행 후 결과 출력
+    */
+}
+
+void Application::WriteAll(int value)
+{
+    /*
+        ✔ WriteAll(value)
+        ▪ 0x00 ~ 0x04 까지 모두 value 값으로 Write한다.
+    */
+}
---
+++ DeviceDriver/Application.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Application
+{
+public:
+    void ReadAndPrint(long startAddr, long endAddr);
+    void WriteAll(int value);
+};
