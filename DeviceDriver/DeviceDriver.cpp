528cb452a61b42c7131870064d8d916531d02b2a
junyoung <jun6ak@gmail.com>
2024-07-05 10:10:25 +0900

    [feature] 5번 읽어서 valid value 얻기

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,17 +1,30 @@
 #include "DeviceDriver.h"
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    int data = 0;
+    vector<int> data;
+
     for (int i = 0; i < 5; i++)
     {
-        data = (int)(m_hardware->read(address));
+        data.push_back((int)(m_hardware->read(address)));
     }
 
-    return data;
+    int comp = data[0];
+    for (int& value : data)
+    {
+        if (value == comp) continue;
+
+        throw runtime_error("[READ ERROR] Flash Memory is not unstable.");
+    }
+
+    return comp;
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,6 +2,11 @@
 #include "MockedFlashMemoryDevice.cpp"
 #include "../DeviceDriver/DeviceDriver.cpp"
 
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
 class DeviceDriverTest : public testing::Test
 {
 protected:
@@ -12,9 +17,46 @@ protected:
 TEST_F(DeviceDriverTest, ReadFiveTimes) {
     long address = 0xA0;
 
-    EXPECT_CALL(mockedFlash, getData(testing::_)).Times(5).WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(mockedFlash, getData(testing::_))
+        .Times(5)
+        .WillRepeatedly(testing::Return(0));
 
-    deviceDriver.read(address);
-
+    EXPECT_EQ(deviceDriver.read(address), 0);
     EXPECT_EQ(mockedFlash.getReadCount(), 5);
+}
+
+TEST_F(DeviceDriverTest, ReadValidData) {
+    long address = 0xA0;
+
+    EXPECT_CALL(mockedFlash, getData(testing::_))
+        .Times(5)
+        .WillRepeatedly(testing::Return(0));
+
+    try
+    {
+        EXPECT_EQ(deviceDriver.read(address), 0);
+    }
+    catch (...)
+    {
+        FAIL();
+    }
+}
+
+TEST_F(DeviceDriverTest, ReadInvalidData) {
+    long address = 0xA0;
+
+    EXPECT_CALL(mockedFlash, getData(testing::_))
+        .Times(5)
+        .WillOnce(testing::Return(1))
+        .WillRepeatedly(testing::Return(0));
+
+    try
+    {
+        int data = deviceDriver.read(address);
+        FAIL();
+    }
+    catch (exception& e)
+    {
+        EXPECT_EQ(string("[READ ERROR] Flash Memory is not unstable."), string(e.what()));
+    }
 }
